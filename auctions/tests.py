358c67f5473da81c111e00600d9b5e59fa89e240
BobsProgrammingAcademy <bobsprogrammingacademy@gmail.com>
2023-05-15 16:05:33 +0200

    Added unit tests

--- auctions/tests.py
+++ auctions/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import User
 
-# Create your tests here.
+
+class UserModelUnitTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(email='john@test.com', username='John', password='pass123')
+
+    def test_user_model(self):
+        data = self.user
+        self.assertTrue(isinstance(data, User))
+        self.assertIsInstance(data, User)
+        self.assertEqual(str(data.username), 'John')
