9d8187dc02217e88a7ebb036cc7348197ec4a748
BobsProgrammingAcademy <bobsprogrammingacademy@gmail.com>
2023-05-15 16:11:59 +0200

    Updated unit tests

--- auctions/tests.py
+++ auctions/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .models import User
+from .models import User, Category
 
 
 class UserModelUnitTestCase(TestCase):
@@ -11,3 +11,13 @@ class UserModelUnitTestCase(TestCase):
         self.assertTrue(isinstance(data, User))
         self.assertIsInstance(data, User)
         self.assertEqual(str(data.username), 'John')
+
+
+class CategoryUnitTestCase(TestCase):
+    def setUp(self):
+        self.category = Category.objects.create(category_name='Books')
+
+    def test_food_category_model(self):
+        data = self.category
+        self.assertIsInstance(data, Category)
+        self.assertEqual(str(data.category_name), 'Books')
